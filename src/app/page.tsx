d52f038ad60ac412d815ba78684c9a80476f611f
Promepy <murahariraogowtham2000@gmail.com>
2024-02-23 23:44:37 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Form from "@/components/Form"
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-black p-24">
+      <Form/>
+    </main>
+  );
+}
